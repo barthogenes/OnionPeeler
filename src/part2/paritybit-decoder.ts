b59dae4975fc1fe0fb4371367109bec6e6a56735
Bart Hogenes <barthogenes@gmail.com>
2021-09-06 11:00:24 +0200

    Cleaned up paritybit-decoder.ts

--- src/part2/paritybit-decoder.ts
+++ src/part2/paritybit-decoder.ts
@@ -11,20 +11,16 @@ export const paritybitDecode = (inputStr: string): string => {
     }
   }
 
-  const bytes = cutUpString(validBytes, 8);
-  return bytes.map(x => {
-    return String.fromCharCode(parseInt(x, 2))
-  }).join('');
+  return cutUpString(validBytes, 8).map(x => String.fromCharCode(parseInt(x, 2))).join('');
 }
 export const parityCheck = (val: number): string => {
-  const bitStrArray = val.toString(2).padStart(8, '0').split('');
-  const databits = bitStrArray.slice(0, bitStrArray.length - 1);
-  const paritybit = bitStrArray.slice(bitStrArray.length - 1, bitStrArray.length);
-  const bitCount = databits.reduce((prev, curr) => (curr === '1' ? prev + 1 : prev), 0);
+  const parity = val & 0b00000001;
+  const data = val & 0b11111110;
+  const bitCount = data.toString(2).split('').reduce((prev, curr) => (curr === '1' ? prev + 1 : prev), 0);
   const isEven = bitCount % 2 === 0;
   const isOdd = bitCount % 2 !== 0;
-  if ((isEven && paritybit[0] === '0') || (isOdd && paritybit[0] === '1')) {
-    return databits.join('');
+  if ((isEven && !parity) || (isOdd && parity)) {
+    return (data >> 1).toString(2).padStart(7, '0');
   }
   return null;
 }
